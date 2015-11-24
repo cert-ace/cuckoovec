883a2cd91e645cbddb8e92c4fed4f49593f48b84
Shiva Kaul <shiva.kaul@gmail.com>
2015-11-23 21:02:31 -0500

    Delta generation updated.

--- deltas.py
+++ deltas.py
@@ -1,3 +1,8 @@
+import os, sys, glob
+import numpy as np
+
+from scipy.misc import imsave
+
 def delta(m1, m2):
   d = {}
   for k in set().union(m1, m2):
@@ -8,3 +13,22 @@ def delta(m1, m2):
 
 def deltas(M): 
   return [delta(m1,m2) for m1,m2 in zip(M[:],M[1:])] 
+  
+def readDetections(folder):
+  T = {}
+  for name in glob.glob(folder + '/result_*.txt'):
+    i = name.split('_')[1].split('.')[0]
+    print(i)
+    with open(os.path.join(folder, name)) as file:
+      T[int(i)] = {chunks[3]: float(chunks[0]) for chunks in [line.split('\t') for line in file if line]}
+  return T
+  
+
+folder = sys.argv[1]
+T = readDetections(folder)
+M = [v for (i,v) in sorted(T.items())]
+templateNames = set().union(*M)
+D = deltas(M)
+A = np.asmatrix([[d.get(name) or 0 for name in templateNames] for d in D])
+#A = np.array()
+imsave('deltas.png', A)
